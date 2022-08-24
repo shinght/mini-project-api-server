1e8dc8ffa3dfd7c521404a40fd234fc5d43fae75
shinght <111275618+shinght@users.noreply.github.com>
2022-08-24 16:05:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let amount = require('./amount')
+let cors = require('cors')
+
+// create express app
+let app = express()
+// configure for cors
+app.use(cors())
+app.use(express.json())
+
+// connect to mongodb using mongoose
+mongoose.connect("mongodb://localhost:27017/bellsandwhistles")
+let db = mongoose.connection
+// check if connection was success
+db.once("open", ()=>{
+    console.log("Connected to mongodb database!");
+})
+
+// welcome api
+app.get("/", (req, res)=>{
+    res.json({
+        message: "Welcome to Bells & Whistles API"
+    })
+})
+
+// get the list of all bells and whistles for all friends
+app.get("/get/friends", (req, res)=>{
+    amount.find({}, (error, data)=>{
+        if (error) {
+            res.json(error)
+        }else {
+            console.log(data)     //you should not log in real application because it takes time
+            res.json(data)
+        }
+    })
+    
+    // res.json({
+    //     message: "Get the list of all bells and whistles for all friends"
+    // })
+})
+
+// add new friend to mongodb database
+app.post("/add/friend", (req, res)=>{
+    // console.log(req);
+    console.log(req.body);
+    // create new amount model instance
+    let newFriend = new amount()
+    newFriend.name = req.body.name
+    newFriend.bells = req.body.bells
+    newFriend.whistles = req.body.whistles
+    newFriend.save((error, result)=>{
+        if (error){
+            res.json(error)
+        }else{
+            res.json(result)
+        }
+    })
+
+    // res.json({
+    //     message: "add is success"
+    // })
+})
+
+
+// make this app listen to port 4211
+let PORT = 4211
+
+// if success, print in console
+app.listen(PORT, ()=>{
+    console.log(`Listening to port ${PORT}`)
+})
